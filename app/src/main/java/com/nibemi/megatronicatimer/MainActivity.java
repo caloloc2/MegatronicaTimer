043dea0f987d279e7bd35280defc0da60e533f8e
caloloc2 <calolomino@gmail.com>
2019-02-17 22:24:07 -0500

    Actualizacion

--- app/src/main/java/com/nibemi/megatronicatimer/MainActivity.java
+++ app/src/main/java/com/nibemi/megatronicatimer/MainActivity.java
@@ -1,28 +1,331 @@
 package com.nibemi.megatronicatimer;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.os.Build;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Size;
+import android.util.SparseIntArray;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Surface;
+import android.view.TextureView;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+@TargetApi(Build.VERSION_CODES.LOLLIPOP)
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CAMERA = 1;
     private static final int REQUEST_BLUETOOTH = 0;
-    AlertDialog dialogo;
+
+    Bluetooth bluetooth = new Bluetooth();
+    private AlertDialog dialogo;
+
+    private Button boton_grabar;
+    private TextureView visor_camara;
+    private TextureView.SurfaceTextureListener mSurfaceTextureListener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+            SetupCamara(width, height);
+            ConnectCamera();
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+        }
+    };
+    private CameraDevice mCameraDevice;
+    private CameraDevice.StateCallback mCameraDeviceStateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            mCameraDevice = camera;
+            StartPreview();
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+            camera.close();
+            mCameraDevice = null;
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {
+            camera.close();
+            mCameraDevice = null;
+        }
+    };
+    private HandlerThread mBackgroundHandlerThread;
+    private Handler mBackgroundHandler;
+    private String mCameraId;
+    private Size mPreviewSize;
+    private CaptureRequest.Builder mCaptureRequestBuilder;
+    private static SparseIntArray ORIENTATIONS = new SparseIntArray();
+    static{
+        ORIENTATIONS.append(Surface.ROTATION_0, 0);
+        ORIENTATIONS.append(Surface.ROTATION_90, 90);
+        ORIENTATIONS.append(Surface.ROTATION_180, 180);
+        ORIENTATIONS.append(Surface.ROTATION_270, 270);
+    }
+
+    private static class CompareSizesbyArea implements Comparator<Size>{
+
+        @Override
+        public int compare(Size o1, Size o2) {
+            return Long.signum((long) o1.getWidth() * o1.getHeight() /
+                    (long) o2.getWidth() * o2.getHeight());
+        }
+    }
+
+    private void SetupCamara(int width, int height){
+        CameraManager cameraManager = (CameraManager)getSystemService(Context.CAMERA_SERVICE);
+        try {
+            for(String cameraId : cameraManager.getCameraIdList()){
+                CameraCharacteristics cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraId);
+                if (cameraCharacteristics.get(CameraCharacteristics.LENS_FACING) ==
+                        CameraCharacteristics.LENS_FACING_FRONT){
+                    continue;
+                }
+                StreamConfigurationMap map = cameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+
+                int deviceOrientation = getWindowManager().getDefaultDisplay().getRotation();
+                int totalRotation = sensorToDeviceRotation(cameraCharacteristics, deviceOrientation);
+                boolean swapRotation =  totalRotation == 90 || totalRotation == 270;
+                int rotatedWidth = width;
+                int rotatedHeight = height;
+
+                if (swapRotation){
+                    rotatedWidth = height;
+                    rotatedHeight = width;
+                }
+                mPreviewSize = chooseOptimalSize(map.getOutputSizes(SurfaceTexture.class), rotatedWidth, rotatedHeight);
+
+                mCameraId = cameraId;
+                return;
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void ConnectCamera(){
+        CameraManager cameraManager = (CameraManager)getSystemService(Context.CAMERA_SERVICE);
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+                    cameraManager.openCamera(mCameraId, mCameraDeviceStateCallback, mBackgroundHandler);
+                }else{
+                    if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)){
+                        Toast.makeText(getApplicationContext(), "Se necesita de acceso a la camara.",Toast.LENGTH_SHORT).show();
+                    }
+                    requestPermissions(new String[] {Manifest.permission.CAMERA}, REQUEST_CAMERA);
+                }
+            }else{
+                cameraManager.openCamera(mCameraId, mCameraDeviceStateCallback, mBackgroundHandler);
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void StartPreview(){
+        SurfaceTexture surfaceTexture = visor_camara.getSurfaceTexture();
+        surfaceTexture.setDefaultBufferSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
+        Surface previewSurface = new Surface(surfaceTexture);
+
+        try {
+            mCaptureRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            mCaptureRequestBuilder.addTarget(previewSurface);
+
+            mCameraDevice.createCaptureSession(Arrays.asList(previewSurface), new CameraCaptureSession.StateCallback() {
+                @Override
+                public void onConfigured(@NonNull CameraCaptureSession session) {
+                    try {
+                        session.setRepeatingRequest(mCaptureRequestBuilder.build(), null, mBackgroundHandler);
+
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+                    Toast.makeText(getApplicationContext(), "No se pudo crear una vista previa de la camara", Toast.LENGTH_SHORT).show();
+                }
+            }, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void CloseCamara(){
+        if (mCameraDevice!=null){
+            mCameraDevice.close();
+            mCameraDevice = null;
+        }
+    }
+
+    private void StartBackgroundThread(){
+        mBackgroundHandlerThread = new HandlerThread("Camara2VideoImage");
+        mBackgroundHandlerThread.start();
+        mBackgroundHandler = new Handler(mBackgroundHandlerThread.getLooper());
+    }
+
+    private void StopBackgroundThread(){
+        mBackgroundHandlerThread.quitSafely();
+        try {
+            mBackgroundHandlerThread.join();
+            mBackgroundHandlerThread = null;
+            mBackgroundHandler = null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static int sensorToDeviceRotation(CameraCharacteristics cameraCharacteristics, int deviceOrientation){
+        int  sensorOrientation = cameraCharacteristics.get(CameraCharacteristics.SENSOR_ORIENTATION);
+        deviceOrientation = ORIENTATIONS.get(deviceOrientation);
+        return (sensorOrientation+deviceOrientation+360) % 360;
+    }
+
+    private static Size chooseOptimalSize(Size[] choices, int width, int height){
+        List<Size> bigEnough = new ArrayList<Size>();
+        for(Size option : choices){
+            if (option.getHeight() == option.getWidth() * height * width &&
+                    option.getWidth()>=width && option.getHeight()>= height){
+                bigEnough.add(option);
+            }
+        }
+        if (bigEnough.size()>0){
+            return Collections.min(bigEnough, new CompareSizesbyArea());
+        }else{
+            return choices[0];
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        checkbluetooth();
+        visor_camara = (TextureView)findViewById(R.id.visor_camara);
+
+
+
+
+
+
+
+
+
+
+
+
+        boton_grabar = (Button)findViewById(R.id.boton_grabar);
+
+        boton_grabar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+
+//        checkbluetooth();
+//
+//        AlertDialog.Builder mBuilder= new AlertDialog.Builder(MainActivity.this);
+//        View mView = getLayoutInflater().inflate(R.layout.conexion_bluetooth, null);
+//
+//        ListView listado_dispositivos = (ListView)findViewById(R.id.listado_dispositivos);
+//        Button conexion_dispositivo =  (Button)mView.findViewById(R.id.conexion_dispositivo);
+//
+//        String[] nombres = bluetooth.getNames();
+//        listado_dispositivos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, nombres));
+//        listado_dispositivos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                if (bluetooth.isConnected()){
+//                    Toast.makeText(getApplicationContext(), "Conectado correctamente", Toast.LENGTH_SHORT).show();
+//                    dialogo.dismiss();
+//                }
+//            }
+//        });
+//
+//        conexion_dispositivo.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                dialogo.dismiss();
+//            }
+//        });
+//
+//        mBuilder.setView(mView);
+//        dialogo = mBuilder.create();
+    }
+
+    @Override
+    protected void onPause() {
+        CloseCamara();
+        StopBackgroundThread();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        StartBackgroundThread();
+
+        if (visor_camara.isAvailable()){
+            SetupCamara(visor_camara.getWidth(), visor_camara.getHeight());
+            ConnectCamera();
+        }else{
+            visor_camara.setSurfaceTextureListener(mSurfaceTextureListener);
+        }
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
@@ -38,21 +341,6 @@ public class MainActivity extends AppCompatActivity {
                     Intent eintent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                     startActivityForResult(eintent, REQUEST_BLUETOOTH);
                 }else{
-                    AlertDialog.Builder mBuilder= new AlertDialog.Builder(MainActivity.this);
-                    View mView = getLayoutInflater().inflate(R.layout.conexion_bluetooth, null);
-
-                    ListView listado_dispositivos = (ListView)findViewById(R.id.listado_dispositivos);
-                    Button conexion_dispositivo =  (Button)mView.findViewById(R.id.conexion_dispositivo);
-
-                    conexion_dispositivo.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            dialogo.dismiss();
-                        }
-                    });
-
-                    mBuilder.setView(mView);
-                    dialogo = mBuilder.create();
                     dialogo.show();
                 }
 
@@ -77,25 +365,20 @@ public class MainActivity extends AppCompatActivity {
         return respuesta;
     }
 
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode==REQUEST_CAMERA){
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(getApplicationContext(), "La aplicacion necesita permisos para acceder a la camara..",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if (resultCode == RESULT_OK){
-
-            AlertDialog.Builder mBuilder= new AlertDialog.Builder(MainActivity.this);
-            View mView = getLayoutInflater().inflate(R.layout.conexion_bluetooth, null);
-
-            ListView listado_dispositivos = (ListView)findViewById(R.id.listado_dispositivos);
-            Button conexion_dispositivo =  (Button)mView.findViewById(R.id.conexion_dispositivo);
-
-            conexion_dispositivo.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    dialogo.dismiss();
-                }
-            });
-
-            mBuilder.setView(mView);
-            dialogo = mBuilder.create();
             dialogo.show();
         }else{
             Toast.makeText(getApplicationContext(), "No se pueden listar dispositivos si no activa el Bluetooth.",Toast.LENGTH_SHORT).show();
